7a11f50ae2c9acee920c51dd974d05e403562529
Igor Basuga <igor.basuga@gmail.com>
2020-02-24 19:05:12 +0000

    added Procfile for deployment

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from flask import Flask, redirect, render_template, request, session, url_for
 
 app = Flask(__name__)
-app.secret_key = "somekey456"
+app.secret_key = os.getenv("SECRET", "somekey456")
 messages = []
 
 
@@ -12,7 +12,7 @@ def add_message(username, message):
     messages.append({"timestamp": now, "from": username, "message": message})
 
 
-@app.route("/", methods = ["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         session["username"] = request.form["username"]
@@ -22,7 +22,8 @@ def index():
 
     return render_template("index.html")
 
-@app.route("/chat/<username>", methods = ["GET", "POST"])
+
+@app.route("/chat/<username>", methods=["GET", "POST"])
 def user(username):
 
     if request.method == "POST":
@@ -31,7 +32,9 @@ def user(username):
         add_message(username, message)
         return redirect(url_for("user", username=session["username"]))
 
-    return render_template("chat.html", username=username, chat_messages=messages)
+    return render_template("chat.html", username=username,
+                           chat_messages=messages)
 
 
-app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)
+app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", "5000")),
+        debug=False)
